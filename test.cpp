8564291cf800f52eaf80f625608f976d8a758e64
xiaomo9 <aochen0315@163.com>
2024-09-22 12:39:15 +0800

    add

--- test.cpp
+++ test.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 using namespace std;
 #include<time.h>
+#include<vector>
+#include<algorithm>
 
 class Solution {
 public:
-    vector<int> countBits(int n) {
-        int count
-        while(n != 0)
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> v;
+        for(int i = 0; i<nums1.size(); i++)
+        {
+            if(find(nums2.begin(),nums2.end(),nums1[i])!=nums2.end())
+            {
+                
+                
+            }
+        }
+        return v;
     }
 };
+
+int main()
+{
+    system("pause");
+}
