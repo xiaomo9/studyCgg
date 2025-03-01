079f1ea82ec8ce92abf1f86a94c1253481cd360e
xiaomo9 <aochen0315@163.com>
2025-03-01 17:03:28 +0800

    测试1.0版本回退

---
+++ 测试.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int fun(int a,int b){
+    return a+b;
+}
+
+int main(){
+    int a = 1, b = 2;
+    cout << fun(a,b) << endl;
+    return 0;
+    system("pause");
+}
